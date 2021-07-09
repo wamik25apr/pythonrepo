011bf65bb2620ae70d91abf22404b66f6245d97e
wamik25apr <wamik25apr@gmail.com>
2021-07-09 17:17:01 +0530

    first push

---
+++ main.py
@@ -0,0 +1,12 @@
+name=input('what is your name?? ')
+print('helooo '+name)
+print(round(10.6))
+print(abs(-10.6))
+test1='this is a string'
+print(len(test1))
+print(test1[:5])
+print(test1.capitalize())
+print(test1.upper())
+print(f'this is a goof thing {test1}')
+print('this is a {} good boy{}'.format(test1,test1))
+
